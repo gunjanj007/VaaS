75dad53d6469e031b370170c92ec334c7042881e
Gunjan Jalori <gunjanjalori@google.com>
2025-06-07 16:42:20 -0700

    commit frondend skel

--- vaasfront/tailwind.config.ts
+++ vaasfront/tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}", // This line is crucial
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/*.{js,ts,jsx,tsx,mdx,css,ico}", // This line is crucial
   ],
   theme: {
     extend: {
